385159ff04ed170284fcfee156b9578f24b7976a
Santiago <santiago.chamon@gmail.com>
2019-10-26 11:53:33 +1000

    fixed my pug error

--- pinkNinjas/routes/index.js
+++ pinkNinjas/routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+//- Require controller modules
+var user_controller = require('../controllers/userController');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'pinkNinjas',
